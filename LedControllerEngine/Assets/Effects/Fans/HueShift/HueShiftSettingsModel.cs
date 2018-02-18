0a36e81547f3eba172b01c5b00d08146c14b05a9
Omar Destefani <omar.destefani@outlook.com>
2018-02-18 19:59:51 +0100

    add new settings to Fan Hue effect

--- LedControllerEngine/Assets/Effects/Fans/HueShift/HueShiftSettingsModel.cs
+++ LedControllerEngine/Assets/Effects/Fans/HueShift/HueShiftSettingsModel.cs
@@ -5,6 +5,7 @@
         public int? StartingHue{ get; set; }
         public int? EndingHue { get; set; }
         public int? HueOffset { get; set; }
+        public int? StartingSaturation { get; set; }
         public int? PhaseOffset { get; set; }
         public int? Speed { get; set; }
     }
